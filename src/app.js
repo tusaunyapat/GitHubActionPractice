3801ea5b1be32a596ab71949e524f72ee8266e3c
Aunyapat Nitijarasrat <aunyapat2003@gmail.com>
2025-04-05 22:35:18 +0700

    update app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    return days[date.getDay()];
-}
+  return days[date.getDay()];
+};
 
+const monthOfTheYear = (date = new Date()) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return months[date.getMonth()];
+};
 
 try {
-    document.getElementById('day').innerText = dayOfTheWeek();
-} catch(err) {}
-
+  document.getElementById("day").innerText = dayOfTheWeek();
+} catch (err) {}
 
 exports.dayOfTheWeek = dayOfTheWeek;
