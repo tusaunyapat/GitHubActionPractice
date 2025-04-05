5450ca8a90a04923fa08608877fa4d05c117eb69
Aunyapat Nitijarasrat <aunyapat2003@gmail.com>
2025-04-05 23:13:15 +0700

    add config

---
+++ config.js
@@ -0,0 +1,4 @@
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
